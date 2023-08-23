5a02689d3fac112f4317cf4142c0a907a769f69b
Martin Choy <mtchoy@comp.hkbu.edu.hk>
2023-08-23 13:52:55 +0800

    Add timeslot/booking api

--- routes/timeslots.js
+++ routes/timeslots.js
@@ -41,4 +41,56 @@ router.get('/', async (req, res) => {
     }
 });
 
+
+// Get timeslots and their bookings, by date, sorted by room, and then start time
+router.get('/bookings', async (req, res) => {
+
+    const { date } = req.query;
+
+    if (!date) {
+        res.status(400).json({ message: "Date is required" });
+        return;
+    }
+
+    var startDate = new Date(date);
+    var endDate = addDays(startDate, 1)
+
+    // var startDate = new Date();
+    // var endDate = addDays(startDate, 1)
+    // // endDate.setDate(startDate.getDate() + 1) // 1 day
+
+    const query = { startTime: { $gt: startDate }, endTime: { $lt: endDate }, status: { $in: ["Approved", "Blocked"] } };
+
+    const db = await connectToDB();
+    try {
+        var pipeline = [
+            {
+                $match: query
+            },
+            {
+                $lookup: {
+                    from: "booking",
+                    localField: "booking",
+                    foreignField: "_id",
+                    as: "bookings"
+                }
+            },
+            {
+                $sort: { 
+                    room: 1,
+                    startTime: 1 
+                }
+            }
+        ]
+
+        const result = await db.collection('timeslot').aggregate(pipeline).toArray();
+        res.status(200).json(result);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    } finally {
+        await db.client.close();
+    }
+});
+
+
 module.exports = router;
