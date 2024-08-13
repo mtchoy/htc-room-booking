974f6f5da79f7020b6ba791d0f67e73eb1d6062b
Martin Choy <mtchoy@comp.hkbu.edu.hk>
2024-08-13 17:43:09 +1000

    Barchart shows 7-18 globally

--- routes/timeslots.js
+++ routes/timeslots.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 const { addDays, parseISO } = require('date-fns')
-// const checkToken = require('../middlewares/checkToken');
 const { connectToDB, ObjectId } = require('../utils/db');
 
 // Get all timeslot item
@@ -15,17 +14,14 @@ router.get('/', async (req, res) => {
         return;
     }
 
-    // var startDate = new Date(date);
-    var startDate = parseISO(date);
+    var startDate = new Date(date);
     const query = { startTime: { $gt: startDate } };
 
     if (room) {
         query.room = room;
         var endDate = addDays(startDate, 31)
-        // endDate.setDate(startDate.getDate() + 31) // 31 days
     } else {
         var endDate = addDays(startDate, 1)
-        // endDate.setDate(startDate.getDate() + 1) // 1 day
     }
 
     query.endTime = { $lt: endDate }
@@ -56,10 +52,6 @@ router.get('/bookings', async (req, res) => {
     // var startDate = new Date(date);
     var startDate = parseISO(date);
     var endDate = addDays(startDate, 1)
-
-    // var startDate = new Date();
-    // var endDate = addDays(startDate, 1)
-    // // endDate.setDate(startDate.getDate() + 1) // 1 day
 
     const query = { startTime: { $gt: startDate }, endTime: { $lt: endDate }, status: { $in: ["Approved", "Blocked"] } };
 
